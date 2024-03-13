cb1098dfbe595d96a8a4e6b148bac89740e5f0ac
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-03-13 21:45:18 +0700

    starter project for learn advanced navigation redirection

--- advanced_navigation_redirection/lib/main.dart
+++ advanced_navigation_redirection/lib/main.dart
@@ -1,19 +1,38 @@
+
+import 'package:advanced_navigation_redirection/routes/router_delegate.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const QuotesApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class QuotesApp extends StatefulWidget {
+  const QuotesApp({super.key});
+
+  @override
+  State<QuotesApp> createState() => _QuotesAppState();
+}
+
+class _QuotesAppState extends State<QuotesApp> {
+  late MyRouterDelegate myRouterDelegate;
+
+  @override
+  void initState() {
+    super.initState();
+    myRouterDelegate = MyRouterDelegate();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Quotes App',
+
+      /// todo 4: change Navigator widget to Router widget
+      home: Router(
+        routerDelegate: myRouterDelegate,
+
+        /// todo 5: add backButtonnDispatcher to handle System Back Button
+        backButtonDispatcher: RootBackButtonDispatcher(),
       ),
     );
   }

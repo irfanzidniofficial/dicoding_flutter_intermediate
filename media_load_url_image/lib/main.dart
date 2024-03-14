ae7fe006bb40e3c2e30a7d5f7a7962a27aaa9c47
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-03-14 22:10:25 +0700

    learn media load url image handle cache from internet

--- media_load_url_image/lib/main.dart
+++ media_load_url_image/lib/main.dart
@@ -1,18 +1,98 @@
+import 'dart:convert';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Learn Media'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FadeInImage.assetNetwork(
+                placeholder: 'assets/blocks.gif',
+                image: 'https://picsum.photos/300/300',
+                fadeOutDuration: const Duration(seconds: 2),
+                fadeInDuration: const Duration(seconds: 2),
+              ),
+
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Divider(
+                  thickness: 1,
+                ),
+              ),
+
+              FutureBuilder<String>(
+                future: rootBundle.loadString("assets/image-base64.txt"),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return FadeInImage.memoryNetwork(
+                      placeholder: base64.decode(snapshot.data!),
+                      image: 'https://picsum.photos/300/300',
+                    );
+                  }
+                  return const SizedBox();
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Divider(
+                  thickness: 1,
+                ),
+              ),
+
+              // Untuk mendapatkan gambar dari internet
+              CachedNetworkImage(
+                cacheKey: "cache-key",
+                imageUrl: 'https://picsum.photos/300/300',
+                // placeholder: (context, url) => Image.asset("assets/blocks.gif"),
+                progressIndicatorBuilder: (context, url, progress) =>
+                    CircularProgressIndicator(value: progress.progress),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+                // untuk mengatur cache
+                cacheManager: CacheManager(
+                  Config(
+                    "cache-key-name",
+                    stalePeriod:
+                        const Duration(seconds: 3), // disimpan selama 3 detik
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

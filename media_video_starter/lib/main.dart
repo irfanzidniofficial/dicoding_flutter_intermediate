62d11d3cab5d89289155f85ef29e7aa76e5e4a62
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-03-15 08:53:57 +0700

    create starter project media_video_starter

--- media_video_starter/lib/main.dart
+++ media_video_starter/lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'provider/video_notifier.dart';
+import 'screen/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return ChangeNotifierProvider(
+      create: (context) => VideoNotifier(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.dark(),
+        home: const HomeScreen(),
       ),
     );
   }

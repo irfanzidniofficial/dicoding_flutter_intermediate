e5c2e8a1fc08fc9e84363aadafad389733561c48
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-03-15 10:46:11 +0700

    learn video player video_player package

--- media_video/lib/screen/home_screen.dart
+++ media_video/lib/screen/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:media_video/provider/video_notifier.dart';
+import 'package:media_video/utils/util.dart';
 import 'package:media_video/widgets/buffer_slider_controller_widget.dart';
 import 'package:media_video/widgets/video_controller_widget.dart';
-
+import 'package:provider/provider.dart';
+import 'package:video_player/video_player.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,6 +14,58 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  VideoPlayerController? controller;
+  bool isVideoInitialize = false;
+
+  void videoInitialize() async {
+    final previousVideoController = controller;
+    // Asset Internal
+    // final videoController = VideoPlayerController.asset(
+    //   'assets/butterfly.mp4',
+    // );
+    // Network
+    final videoController = VideoPlayerController.networkUrl(
+      Uri.parse(
+          "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"),
+    );
+
+    await previousVideoController?.dispose();
+
+    try {
+      await videoController.initialize();
+    } on Exception catch (e) {
+      print('Error initializing video: $e');
+    }
+
+    if (mounted) {
+      setState(() {
+        controller = videoController;
+        isVideoInitialize = controller!.value.isInitialized;
+      });
+
+      if (isVideoInitialize) {
+        final provider = context.read<VideoNotifier>();
+        controller?.addListener(() {
+          provider.duration = controller?.value.duration ?? Duration.zero;
+          provider.position = controller?.value.position ?? Duration.zero;
+          provider.isPlay = controller?.value.isPlaying ?? false;
+        });
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    videoInitialize();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,24 +75,48 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Stack(
         alignment: Alignment.center,
         children: [
-          const CircularProgressIndicator(),
+          isVideoInitialize
+              ? AspectRatio(
+                  aspectRatio: controller!.value.aspectRatio,
+                  child: VideoPlayer(controller!),
+                )
+              : const CircularProgressIndicator(),
           Container(
             padding: const EdgeInsets.symmetric(vertical: 16),
             alignment: Alignment.bottomCenter,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                BufferSliderControllerWidget(
-                  maxValue: 5,
-                  currentValue: 1,
-                  minText: "Current Duration",
-                  maxText: "Maximum Duration",
-                  onChanged: (value) async {},
+                Consumer<VideoNotifier>(
+                  builder: (context, provider, child) {
+                    final duration = provider.duration;
+                    final position = provider.position;
+                    return BufferSliderControllerWidget(
+                      maxValue: duration.inSeconds.toDouble(),
+                      currentValue: position.inSeconds.toDouble(),
+                      minText: durationToTimeString(position),
+                      maxText: durationToTimeString(duration),
+                      onChanged: (value) async {
+                        final newPosition = Duration(seconds: value.toInt());
+                        await controller?.seekTo(newPosition);
+                        await controller?.play();
+                      },
+                    );
+                  },
                 ),
-                VideoControllerWidget(
-                  onPlayTapped: () {},
-                  onPauseTapped: () {},
-                  isPlay: false,
+                Consumer<VideoNotifier>(
+                  builder: (context, provider, child) {
+                    final isPlay = provider.isPlay;
+                    return VideoControllerWidget(
+                      onPlayTapped: () {
+                        controller?.play();
+                      },
+                      onPauseTapped: () {
+                        controller?.pause();
+                      },
+                      isPlay: isPlay,
+                    );
+                  },
                 ),
               ],
             ),

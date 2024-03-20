1f104f41fe6a4009d6fcbcfdbf9d80fec7619704
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-03-21 06:10:09 +0700

    first commit copy repo from advanced_widget_explicit_starter

---
+++ advanced_widget_explicit_animation_final/README.md
@@ -0,0 +1,3 @@
+# advanced_widget_explicit_animation_final
+
+A new Flutter project.
---
+++ advanced_widget_explicit_animation_final/android/app/src/main/kotlin/com/example/advanced_widget_explicit_animation_final/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.advanced_widget_explicit_animation_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

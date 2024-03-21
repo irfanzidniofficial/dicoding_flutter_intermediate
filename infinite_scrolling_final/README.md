289fc352945d3236e9c2f970f69ef9f3eb206bc2
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-03-21 16:01:52 +0700

    learn infinite scrolling loading

---
+++ infinite_scrolling_final/README.md
@@ -0,0 +1,3 @@
+# infinite_scrolling_final
+
+A new Flutter project.
---
+++ infinite_scrolling_final/android/app/src/main/kotlin/com/example/infinite_scrolling_final/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.infinite_scrolling_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

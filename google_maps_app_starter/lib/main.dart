4a37a9faabd5770c00662b63be7846ae2839dd20
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-03-22 13:06:00 +0700

    create starter project google maps

--- google_maps_app_starter/lib/main.dart
+++ google_maps_app_starter/lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'screen/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const HomeScreen(),
     );
   }
 }

f529158fb19bde2399c45abce0eb00424254aa04
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-03-22 10:08:29 +0700

    learn code generation json serializable

--- code_generation_json_serializable_final/lib/model/quote.dart
+++ code_generation_json_serializable_final/lib/model/quote.dart
@@ -1,3 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'quote.g.dart';
+
+@JsonSerializable()
 class Quote {
   final String id;
   final String en;
@@ -11,19 +16,7 @@ class Quote {
     required this.rating,
   });
 
-  factory Quote.fromJson(Map<String, dynamic> map) {
-    return Quote(
-      id: map['id'] ?? '',
-      en: map['en'] ?? '',
-      author: map['author'] ?? '',
-      rating: map['rating']?.toDouble() ?? 0.0,
-    );
-  }
+  factory Quote.fromJson(Map<String, dynamic> json) => _$QuoteFromJson(json);
 
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'en': en,
-        'author': author,
-        'rating': rating,
-      };
+  Map<String, dynamic> toJson() => _$QuoteToJson(this);
 }

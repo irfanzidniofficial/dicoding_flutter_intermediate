2aed5baa85fb6eaaa7823812c8c73f2d723f0063
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-03-23 11:44:01 +0700

    learn location maps

--- google_maps_app_picker/lib/screen/picker_screen.dart
+++ google_maps_app_picker/lib/screen/picker_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class PickerScreen extends StatefulWidget {
   const PickerScreen({super.key});
@@ -8,12 +10,110 @@ class PickerScreen extends StatefulWidget {
 }
 
 class _PickerScreenState extends State<PickerScreen> {
+  final dicodingOffice = const LatLng(-6.8957473, 107.6337669);
+
+  late GoogleMapController mapController;
+
+  late final Set<Marker> markers = {};
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Maps"),
+        child: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: dicodingOffice,
+                zoom: 18,
+              ),
+              markers: markers,
+              zoomControlsEnabled: false,
+              mapToolbarEnabled: false,
+              myLocationButtonEnabled: false,
+              onMapCreated: (controller) {
+                final marker = Marker(
+                  markerId: const MarkerId("source"),
+                  position: dicodingOffice,
+                );
+
+                setState(() {
+                  mapController = controller;
+                  markers.add(marker);
+                });
+              },
+              myLocationEnabled: true,
+              onLongPress: (LatLng latLng) {
+                onLongPressGoogleMap(latLng);
+              },
+            ),
+            Positioned(
+              top: 16,
+              right: 16,
+              child: FloatingActionButton(
+                onPressed: () {
+                  onMyLocationButtonPress();
+                },
+                child: const Icon(
+                  Icons.my_location,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  void defineMarker(LatLng latLng) {
+    final marker = Marker(
+      markerId: const MarkerId("source"),
+      position: latLng,
+    );
+
+    setState(() {
+      markers.clear();
+      markers.add(marker);
+    });
+  }
+
+  void onMyLocationButtonPress() async {
+    final Location location = Location();
+    late bool serviceEnabled;
+    late PermissionStatus permissionGranted;
+    late LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        print('Location service is not available');
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        print("Location permission granted");
+        return;
+      }
+    }
+
+    locationData = await location.getLocation();
+    final latLng = LatLng(locationData.latitude!, locationData.longitude!);
+    defineMarker(latLng);
+
+    mapController.animateCamera(
+      CameraUpdate.newLatLng(latLng),
+    );
+  }
+
+  void onLongPressGoogleMap(LatLng latLng) async {
+    defineMarker(latLng);
+    mapController.animateCamera(
+      CameraUpdate.newLatLng(latLng),
     );
   }
 }
